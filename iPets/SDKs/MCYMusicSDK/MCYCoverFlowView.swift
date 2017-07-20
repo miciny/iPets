5ebdaee0cda68512c909d4341db4a499451b63d0
miciny <15201114041@163.com>
2017-07-20 12:30:16 +0800

    cover flow done suck

---
+++ iPets/SDKs/MCYMusicSDK/MCYCoverFlowView.swift
@@ -0,0 +1,21 @@
+//
+//  MCYCoverFlowView.swift
+//  iPets
+//
+//  Created by maocaiyuan on 2017/7/18.
+//  Copyright © 2017年 maocaiyuan. All rights reserved.
+//
+
+import UIKit
+
+class MCYCoverFlowView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
